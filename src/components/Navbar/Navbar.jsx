4701668271021ced18baaf217dde28da028a11d7
Oguguom Lawrence <lawrencecally3@gmail.com>
2026-03-19 13:24:26 +0100

    Revamped the menu-icon for the mobile responsiveness

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -15,7 +15,6 @@ const Navbar = () => {
                 <span>FCT</span> <h5>Flux Creative Technologies</h5>
          </div>
 
-         <div className="nav-links">
             <ul className={menuOpen ? "nav-links open" : "nav-links"}>
                 <li><a href="#hero" className='active'>Home</a></li>
                 <li><a href="#about">About</a></li>
@@ -24,7 +23,7 @@ const Navbar = () => {
                 <li><a href="#process">Our Process</a></li>
                 <li><a href="#price">Pricing</a></li>
             </ul>
-        </div>
+        
          
          <div className="contact">
                 <a href='#contact'>Contact Us</a>
